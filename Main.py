dc9c7fe3dbfa7606f449254e0ab561bacd547b6f
Tolga K <tolga_k94@hotmail.com>
2018-02-16 16:27:01 +0100

    güncel profil ismini mail göndertme eklendi

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import bs4 as bs
 import datetime
 import openpyxl
 import difflib
+import smtplib
 from openpyxl import Workbook
 
 sauce = urllib.request.urlopen('https://www.instagram.com/tlgkyck').read()
@@ -47,3 +48,37 @@ else:
 
 kitap.save("insta.xlsx")
 kitap.close()
+
+#####################################################################################
+
+# Hesap bilgilerimiz
+kullanıcı = "mail@gmail.com"
+kullanıcı_sifresi = 'şifre'
+
+alıcı = 'mail'  # alıcının mail adresi
+konu = 'Selam'
+msj = titletext.encode('utf-8')
+
+# bilgileri bir metinde derledik
+email_text = """
+From: {}
+To: {}
+Subject: {}
+{}
+""".format(kullanıcı, alıcı, konu, msj)
+
+try:
+    server = smtplib.SMTP('smtp.gmail.com:587')  # servere bağlanmak için gerekli host ve portu belirttik
+
+    server.starttls()  # serveri TLS(bütün bağlantı şifreli olucak bilgiler korunucak) bağlantısı ile başlattık
+
+    server.login(kullanıcı, kullanıcı_sifresi)  # Gmail SMTP server'ına giriş yaptık
+
+    server.sendmail(kullanıcı, alıcı, email_text)  # Mail'imizi gönderdik
+
+    server.close()  # SMTP serverimizi kapattık
+
+    print('email gönderildi')
+
+except:
+    print("bir hata oluştu")
