9aba8bd392e5e721337eb21f7c22d863432303ee
Tolga K <tolga_k94@hotmail.com>
2018-02-16 01:36:54 +0100

    hata döndürme düzenlemesi yapıldı

--- Main.py
+++ Main.py
@@ -16,36 +16,32 @@ try:
     kitap = openpyxl.load_workbook('insta.xlsx')
     sayfa = kitap.get_sheet_by_name('Sheet')
 
-    print(sayfa)
-
-    A1_verisi = sayfa['A1'].value
-    P1_verisi = sayfa['P1'].value
-
-    if (A1_verisi == None or P1_verisi == None):
-        sayfa['A1'] = sayfa['P1'] = str(h1text)
-        print("Profil isminiz işlenmiştir:")
-        print(h1text)
-
-    else:
-        cell = sayfa['P1'].value
-        print(cell)
-
-        if (cell == h1text):
-            print('Değişiklik yoktur')
-        else:
-            sayfa.append([h1text])
-
-        sayfa['P1'] = str(h1text)
-        print(str(h1text))
 except IOError as e:
-    print("Kayıt datanız oluşturuldu, lütfen tekrar derleyiniz")
     kitap = Workbook()
     sayfa = kitap.active
     kitap.save("insta.xlsx")
 except ValueError:
-    print("h")
+    print
 
+A1_verisi = sayfa['A1'].value
+P1_verisi = sayfa['P1'].value
 
+if (A1_verisi == None or P1_verisi == None):
+    sayfa['A1'] = sayfa['P1'] = str(h1text)
+    print("Profil ismi işlenmiştir:")
+    print(h1text)
+
+else:
+    cell = sayfa['P1'].value
+    print(cell)
+
+    if (cell == h1text):
+        print('Değişiklik yoktur')
+    else:
+        sayfa.append([h1text])
+
+    sayfa['P1'] = str(h1text)
+    print(str(h1text))
 
 
 
