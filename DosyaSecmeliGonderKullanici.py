d6d0b582431425e6a7e9a53bf7c40ae2f34daa92
Tolga K <tolga_k94@hotmail.com>
2018-02-20 17:35:08 +0100

    kullanıcıdan seçimli input eklendi çoklu database özelliği geldi

--- DosyaSecmeliGonderKullanici.py
+++ DosyaSecmeliGonderKullanici.py
@@ -9,19 +9,52 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from openpyxl import Workbook
 
-webLink = input("Link giriniz: ")
-sendMail= input("Mail adresinizi giriniz: ")
+#webLink = input("Link giriniz: ")
+#sendMail= input("Mail adresinizi giriniz: ")
 
-#webLink= 'https://www.instagram.com/tlgkyck'
-#sendMail= 'tolga_k94@hotmail.com'
+kaynak= int(input("Taranmasını istediğiniz kaynağı seçiniz.\n(1) title\n(2) h1\n(3) h2\n(4) body\n--> "))
+
+webLink= 'https://ubersem.com/'
+sendMail= 'tolga_k94@hotmail.com'
 
 sauce = urllib.request.urlopen(webLink.strip()).read()
 soup = bs.BeautifulSoup(sauce,'lxml')
 
-titledata=soup.title
-titletext=titledata.text
 
-file = "DataBase.xlsx"
+try:
+    titledata = soup.title
+    titletext = titledata.text
+
+    h1data = soup.h1
+    h1text = h1data.text
+
+    h2data = soup.h2
+    h2text = h2data.text
+
+    bodydata = soup.body
+    bodytext = bodydata.text
+except:
+    print("Bu URL, girilen datalara ulaşılmasına izin vermiyor.")
+
+
+
+if (kaynak == 1):
+    data = titletext
+    dataAdi = "title"
+elif (kaynak == 2):
+    data = h1text
+    dataAdi = "h1"
+elif (kaynak == 3):
+    data = h2text
+    dataAdi = "h2"
+elif (kaynak == 4):
+    data = bodytext
+    dataAdi = "body"
+
+print(type(soup.h1))
+print(data)
+
+file = str(dataAdi+"DataBase.xlsx")
 
 def mailGonder (text):
     try:
@@ -62,29 +95,29 @@ except IOError as e: #kayıtlı data yoksa oluştur
     sayfa = kitap.active
     kitap.save(file)
 
-A1_verisi = sayfa['A1'].value #başlangıç verisi
+C1_verisi = sayfa['C1'].value #başlangıç verisi
 Z1_verisi = sayfa['Z1'].value #son veri yedeği
 
-if (A1_verisi == None or Z1_verisi == None): #sütunlar boşsa başlangıç verilerini ekle
-    sayfa['A1'] = sayfa['Z1'] = str(titletext)
-    sayfa['K1'] = datetime.datetime.now()
+if (C1_verisi == None or Z1_verisi == None): #sütunlar boşsa başlangıç verilerini ekle
+    sayfa['C1'] = sayfa['Z1'] = str(data)
+    sayfa['A1'] = datetime.datetime.now()
     textPI= "Veri dosyası oluşturulmuş ve ilk veri işlenmiştir!"
-    print(titletext)
+    print(data)
 
     kitap.save(file)
     kitap.close()
 
     mailGonder(textPI)
 else:
-    if (Z1_verisi == titletext):
+    if (Z1_verisi == data):
         print('Değişiklik yoktur')
     else:
         textPD = "Veri değişikliği algılandı"
-        sayfa.append({'A': titletext, 'K': datetime.datetime.now()})
+        sayfa.append({'C': data, 'A': datetime.datetime.now()})
 
-        sayfa['Z1'] = str(titletext)
+        sayfa['Z1'] = str(data)
 
-        text1_lines = titletext.splitlines()
+        text1_lines = data.splitlines()
         text2_lines = Z1_verisi.splitlines()
 
         d = difflib.Differ()
